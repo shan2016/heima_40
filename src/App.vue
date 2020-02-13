aa534677b2571b421d87fc70a1697d296cbea00b
shan <18312015269@163.com>
2020-02-13 16:47:37 +0800

    02-添加登录的单文件组件并配置好路由，引入vant插件，下一步开始制作登录组件内容

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    aa
     <router-view></router-view>
   </div>
 </template>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+// 这个文件就是封装的路由模块
+
+// 1.引入vue和vue-router
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// 引入路由所映射的组件
+import Login from '@/views/Login.vue'
+// 挂载：让Vue使用VueRouter进行路由管理
+Vue.use(VueRouter)
+
+// 2.创建路由对象
+var router = new VueRouter({
+  // 3.配置路由，通过routes配置路由,它是一个数组，里面的每一个成员都是一个单独的路由对象
+  routes: [
+    // 4.配置具体的路由，一般来说有如下的几个参数
+    // path:路由路径，以后这个值就是url的值
+    // name:路由的名称
+    // component:当前路由所映射的组件，它是一个组件对象，绝对不要写成字符串
+    {
+      name: 'login',
+      path: '/login',
+      component: Login
+    }
+  ]
+})
+
+// 暴露
+export default router
